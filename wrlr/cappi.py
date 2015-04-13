263aaee80787090aa1fea38fedd81cdd7de2ac9a
calliban <badcall0>
2015-04-13 10:27:05 -0300

    Removed Sensors directory and refactored it to base; Limited characters per line to 80 to conform with PEP8

--- wrlr/cappi.py
+++ wrlr/cappi.py
@@ -10,15 +10,15 @@ import gzip
 import numpy as np
 from scipy.ndimage.filters import generic_filter
 
-from sensors.cities import get_city
-from sensors import circles
+import cities
+import circles
 
 
 class CAPPI(object):
     """
     This is the class for working with CAPPI radar files
 
-    :param city: A string for the radar location, such as 'BRU', 'PI', 'PPR', or 'SR'
+    :param city: A string for the radar location
     """
 
     _file_name = ''
@@ -30,20 +30,22 @@ class CAPPI(object):
     steiner_mask = None
 
     def __init__(self, city: str):
+        self.city = cities.cities[city]
 
-        # Validate the city name
-        self.city = get_city(city)
+        lat_line = np.linspace(start=self.city.lat_min,
+                               stop=self.city.lat_max,
+                               num=self.side)
+        lon_line = np.linspace(start=self.city.lon_min,
+                               stop=self.city.lon_max,
+                               num=self.side)
 
-        lat_line = np.linspace(start=self.city.lat_min, stop=self.city.lat_max, num=self.side)
-
-        lon_line = np.linspace(start=self.city.lon_min, stop=self.city.lon_max, num=self.side)
-
-        self.latitude, self.longitude = np.meshgrid(lon_line, lat_line, indexing='ij')
+        self.latitude, self.longitude = np.meshgrid(lon_line, lat_line,
+                                                    indexing='ij')
 
         self.y_size, self.x_size = self.city.shape
         self.y_upper_left, self.x_upper_left = self.city.box_ul
         self.y_lower_right, self.x_lower_right = self.city.box_lr
-        self.data_size = self.x_size * self.y_size * 4  # Binary size for float data radar matrix
+        self.data_size = self.x_size * self.y_size * 4  # Binary size for float
 
     def remap(self, latitude: float, longitude: float) -> list:
         """
@@ -54,11 +56,20 @@ class CAPPI(object):
         :param longitude: The Longitude of the point
         :return: (i, j)
         """
-        new_latitude = round((latitude - self.city.lat_min) * self.city.shape[0] / (
-            self.city.lat_max - self.city.lat_min)) if self.city.lat_min <= latitude < self.city.lat_max else -1
 
-        new_longitude = round((longitude - self.city.lon_min) * self.city.shape[1] / (
-            self.city.lon_max - self.city.lon_min)) if self.city.lon_min <= longitude < self.city.lon_max else -1
+        if self.city.lat_min <= latitude < self.city.lat_max:
+            lat_window = (self.city.lat_max - self.city.lat_min)
+            normal_lat = (latitude - self.city.lat_min) / lat_window
+            new_latitude = round(normal_lat * self.city.shape[0])
+        else:
+            new_latitude = -1
+
+        if self.city.lon_min <= longitude < self.city.lon_max:
+            lon_window = (self.city.lon_max - self.city.lon_min)
+            normal_lon = (longitude - self.city.lon_min) / lon_window
+            new_longitude = round(normal_lon * self.city.shape[1])
+        else:
+            new_longitude = -1
 
         return new_latitude, new_longitude
 
@@ -76,13 +87,15 @@ class CAPPI(object):
         """
         self._file_name = file_name
 
-    def open(self, file_name: str='', remove_borders: bool=True, use_zr: bool=True) -> np.ndarray:
+    def open(self, file_name: str='', remove_borders: bool=True,
+             use_zr: bool=True) -> np.ndarray:
         """
         Open a single radar file given it's file_name".
-        The remove_borders image may be obtained using the "remove_borders" parameter.
+        The remove_borders image may be obtained using the "remove_borders"
+        parameter.
 
         :param file_name: The filename for a radar file
-        :param remove_borders: Define whether to use the inner-radar matrix or not
+        :param remove_borders: Define whether to use the inner-radar matrix
         :param use_zr: Use Z-R (mmh-1)  data if true, or dBZ ir false
         :return cappi_map: The matrix map
         """
@@ -99,45 +112,49 @@ class CAPPI(object):
             del data_stream
             del local_map
 
-        # Bin data files contain a "transparency" value for most data, which must not be used for processing
-        # data. A simple way to avoid using it is to set it to an invalid dBZ value.
-        # A simpler approach is to convert the data to MMH and then change this mask to 0, which is valid.
-        # TODO can I use scikit-image to improve this filtering? This is a project-wide decision!
+        # Bin data files contain a marked value, which must not be used for
+        # processing data. A simple way to avoid using it is to set it to an
+        # invalid dBZ value.
 
         self.mask_value = cappi_map[2, 2]
-        self.mask = cappi_map == self.mask_value  # This is a masked numpy array!
+        self.mask = cappi_map == self.mask_value  # This is a masked numpy array
 
-        # While there may be uses for radar images with the use_zr flag set as false, it is important
-        # to notice this data format is actually dBZ, thus making it difficult some common operations
-        # over the image. This flag is required to be False for the Steiner filtering.
+        # While there may be uses for radar images with the use_zr flag set as
+        # false, it is important to notice this data format is actually dBZ,
+        # thus making it difficult some common operations over the image. This
+        # flag is required to be False for the Steiner filtering.
 
         if use_zr:
             cappi_map[-self.mask] = self.city.zr(cappi_map[-self.mask])
             cappi_map[self.mask] = 0
 
-        # else:
-        #    cappi_map[self.mask] = -999
-
-        # This will reduce the matrix to the square matrix inside the 150 km range of the weather radar.
-        # Without the remove_borders flag active, most of this class won work as planned, so it is
-        # advised not to use it for processing other than the Steiner filtering.
+        # This will reduce the matrix to the square matrix inside the 150 km
+        # range of the weather radar.
+        # Without the remove_borders flag active, most of this class won work as
+        # planned, so it is advised not to use it for processing other than the
+        # Steiner filtering.
 
         if remove_borders:
-            cappi_map = cappi_map[self.y_upper_left:self.y_lower_right, self.x_upper_left:self.x_lower_right]
-            cappi_map = cappi_map[::self.city.y_direction, ::self.city.x_direction]
+            cappi_map = cappi_map[self.y_upper_left:self.y_lower_right,
+                                  self.x_upper_left:self.x_lower_right]
+            cappi_map = cappi_map[::self.city.y_direction,
+                                  ::self.city.x_direction]
             cappi_map[cappi_map < 0] = 0
 
         self.data = cappi_map
 
     def steiner_filter(self, data: np.ndarray):
         """
-        Steiner Filter is based on the Steiner Method Steiner et al. (1995)  for filtering convective rainfall
-        from a radar image.
-        This method follow 3 rules, which should be applied to every point the grid:
+        Steiner Filter is based on the Steiner Method Steiner et al. (1995) for
+        filtering convective rainfall from a radar image.
+        This method follow 3 rules, which should be applied to every point the
+        grid:
 
         1. Intensity: Any point above 40dBZ is a Convective Point
-        2. Peak: Any point above a threshold (a) over the mean local rain (11 km radius circle) is a Convective Point
-        3. Neighbor: Every point around a certain radius (b) around a Convective point is also a Convective Point
+        2. Peak: Any point above a threshold (a) over the mean local rain
+            (11 km radius circle) is a Convective Point
+        3. Neighbor: Every point around a certain radius (b) around a Convective
+            point is also a Convective Point
 
         (a) is the threshold
         (b) is given by the static method convective_radius, in this class
@@ -171,16 +188,19 @@ class CAPPI(object):
     @staticmethod
     def _threshold(background_reflectivity: np.float) -> np.float:
         """
-        Calculates the threshold a certain Z must to be above the background reflectivity in order to
+        Calculates the threshold a certain Z must to be above the background
+        reflectivity in order to
         be considered a Convective Point.
 
         As per Steiner et al. (1995), Equation 2
         :param background_reflectivity:
         """
 
-        # This function will be called several times (as many as there are points in a map), so it should
-        # be as optimal as possible in order to avoid performance issues.
-        # If memory is no issue, maybe a full matrix should be generated here to make use of numpy optimizations.
+        # This function will be called several times (as many as there are
+        # points in a map), so it should be as optimal as possible in order to
+        # avoid performance issues.
+        # If memory is no issue, maybe a full matrix should be generated here to
+        # make use of numpy optimizations.
 
         if background_reflectivity < 0:
             return 10
@@ -191,7 +211,8 @@ class CAPPI(object):
 
     def _above_background(self, data: np.ndarray) -> bool:
         """
-        A filter function to be used with scipy.ndimage.filters.generic_filter in order to find the mean background
+        A filter function to be used with scipy.ndimage.filters.generic_filter
+        in order to find the mean background
         intensity for the entire matrix.
         Returns a boolean np.ndarray with True being the values to filter out.
 
@@ -199,7 +220,8 @@ class CAPPI(object):
         :return:
         """
 
-        # This function will be called about half a million times per image, and should be optimized as much as possible
+        # This function will be called about half a million times per image, and
+        # should be optimized as much as possible
 
         point = data[len(data) // 2]
         if point == self.mask_value:
@@ -212,35 +234,37 @@ class CAPPI(object):
         data = data.mean() if data.size else 0
         return False if point - self._threshold(point) > data else True
 
+    @staticmethod
+    def _convective_radius(reflectivity):
+        """
+        Calculates the Convective Radius of a given convective, i.e. the
+        radius in which all points should be considered convective.
+
+        As per Steiner et al. (1995), Figure 6 (b)
+        :param reflectivity: mean reflectivity over a 11 km radius in dBZ
+        """
+
+        # Tested against a simpler equation ( x//5 - 3) this resulting  formula
+        # had almost twice the performance. This is a function that  will be
+        # called several times, so performance enhancements are welcome.
+
+        if reflectivity < 25:
+            return 1
+        elif reflectivity < 30:
+            return 2
+        elif reflectivity < 35:
+            return 3
+        elif reflectivity < 40:
+            return 4
+        else:
+            return 5
+
     def _surrounding_area(self, data: np.ndarray) -> np.ndarray:
         """
-        This is a semi-optimized class for finding the surrounding area for a given intensity pixel.
+        This is a semi-optimized class for finding the surrounding area for a
+        given intensity pixel.
         :param data:
         """
-
-        def _convective_radius(reflectivity):
-            """
-            Calculates the Convective Radius of a given convective, i.e. the radius in which all points should be
-            considered convective.
-
-            As per Steiner et al. (1995), Figure 6 (b)
-            :param reflectivity: the mean reflectivity over a 11 km radius, in dBZ
-            """
-
-            # Tested against a simpler equation ( x//5 - 3) this resulting formula had almost
-            # twice the performance. This is a function that will be called several times,
-            # so performance enhancements are welcome.
-
-            if reflectivity < 25:
-                return 1
-            elif reflectivity < 30:
-                return 2
-            elif reflectivity < 35:
-                return 3
-            elif reflectivity < 40:
-                return 4
-            else:
-                return 5
 
         output = np.zeros(data.shape, dtype=np.bool)
 
@@ -253,10 +277,12 @@ class CAPPI(object):
 
             if value == self.mask_value:
                 continue
-            if line_min >= line or line >= line_max or column_min >= column or column >= column_max:
+            if line_min >= line or line >= line_max:
+                continue
+            if column_min >= column or column >= column_max:
                 continue
 
-            radius = _convective_radius(value)
+            radius = self._convective_radius(value)
             output[line - radius:line + radius+1, column - radius:column + radius+1] +=\
                 circles.convective_radius[radius]
 
@@ -264,7 +290,8 @@ class CAPPI(object):
 
 
 def main():
-    """This is a completely trivial test that will only run in my machine and should be removed"""
+    """This is a completely trivial test that will only run in my machine and
+    should be removed"""
     # TODO create some real tests
 
     x = CAPPI("BRU")
